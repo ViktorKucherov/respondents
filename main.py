7ea3d13e068cc46e8ea2b5f44ac2c19480f0369b
ViktorKucherov <viktorkucherov@outlook.com>
2021-03-02 17:54:24 +0300

    лабода

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ data_frame_survey = file.parse(file.sheet_names[1])  # парсим объект
 # parser_dict = {year_key: [f"MONTHS:{m}:{year_key}" for m in months] for year_key in years}
 
 
-dictionary = {"Респондент": [], "Вопрос": [], "Ответ": [], "Населенный пункт": [], "Код субъекта": [], "Телефон": []}
+dictionary = {"Респондент": [], "Вопрос": [], "Варианты ответов": [], "Ответ": [], "Населенный пункт": [], "Код субъекта": [], "Телефон": []}
 
 indices = list()
 keys = list()
@@ -22,25 +22,28 @@ for index, row in data_frame_survey.iterrows():
         print(index, ":", row[0])
         indices.append(index)
         keys.append(row[0])
+indices.append(indices[-1]+1)
 
 # print(indices, keys)
 # print({"Вопрос": [key for key in keys]})
-indices.append(indices[-1]+1)
-print(indices)
+questions = [[key] * i for i, key in zip(np.diff(indices), keys)]
+questions = [every_question for sublist in questions for every_question in sublist]
 
 
-list_of_questions = [[key] * i for i, key in zip(np.diff(indices), keys)]
-list_of_questions = [every_question for sublist in list_of_questions for every_question in sublist]
+variants = data_frame_survey[data_frame_survey.columns[1]].values
 
-print(list_of_questions)
-
-list_of_variants = data_frame_survey['Unnamed: 1'].values
-print(len(list_of_variants))
+byte_code = b"\xa0"
 
 
+for variant in variants:
+    print(str(variant))
+    for e in str(variant):
+        print(e)
+        e = re.sub(r'\S+', '', str(e))
+print(variants)
 for column in data_frame_survey.columns[2:]:
-    list_of_answers = data_frame_survey[column].values
-    print(list_of_answers)
+    answers = data_frame_survey[column].values
+
 
 result_df = pd.DataFrame(data=dictionary)
 result_df.to_excel("output.xlsx")
