9a25e8724e2dbdea6719ad83f247ac6658ffacdc
ViktorKucherov <viktorkucherov@outlook.com>
2021-03-04 11:45:22 +0300

    сформировал повторяющийся список респондентов

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import re
 import unicodedata as ud
 
 
-respondents = "respondents.xlsx"  # обозначаем файл
-file = pd.ExcelFile(respondents)  # вызываем метод загрузки файла в объект
+respondents_file = "respondents.xlsx"  # обозначаем файл
+file = pd.ExcelFile(respondents_file)  # вызываем метод загрузки файла в объект
 data_frame_survey = file.parse(file.sheet_names[1])  # парсим объект по необходимому нам листу анкета_опрос
 # print(data_frame_survey.columns) # выводим столбцы
 # data_frame_survey['№ Анкеты']=data_frame_survey['№ Анкеты'].astype('str')
@@ -27,25 +27,26 @@ for index, row in data_frame_survey.iterrows():
         keys.append(row[0])
 indices.append(indices[-1]+1)
 
-# print(indices, keys)
-# print({"Вопрос": [key for key in keys]})
 questions = [[key] * i for i, key in zip(np.diff(indices), keys)]
 questions = [every_question for sublist in questions for every_question in sublist]
-
+count_of_repeatitions = len(questions)
+print(count_of_repeatitions)
 
 variants = data_frame_survey[data_frame_survey.columns[1]].values
-
-
-
-print(f"НЕОТФОРМАТИРОВАННЫЙ ВАРИАНТ! {variants}")
 variants = re.sub(r'(?:\\xa0)+', '', str(variants))
 print(f"ОТФОРМАТИРОВАННЫЙ ВАРИАНТ! {variants}")
 
+respondents = list()
+respondents_names = data_frame_survey.columns[2:]
+respondents = []
 
-for column in data_frame_survey.columns[2:]:
-    answers = data_frame_survey[column].values
+for respondent_name in respondents_names:
+    respondents = respondents + [respondent_name]*count_of_repeatitions
+    #for e in range(count_of_repeatitions):
+    #    respondents.append(respondent_name)
+
+print(len(respondents))
 
 
 result_df = pd.DataFrame(data=dictionary)
 result_df.to_excel("output.xlsx")
-print(0,"O")
